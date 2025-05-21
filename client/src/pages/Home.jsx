e85c65dd9e78f9cabe30284c0340a25d2c6e229a
unknown <dashulja861@mail.com>
2025-05-21 20:51:46 +0100

    restructuring

--- client/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h2>Welcome to Event App</h2>
-            <p>Search and save your favorite events. Login or Register to get started.</p>
-        </div>
-    );
-}
