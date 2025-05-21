9b1a1354f73ded996a4e0acb925625d65f5cad47
emifirmat <emifirmat_cp@hotmail.com>
2025-05-21 13:57:34 +0100

    Added basic login, register, and resetPassword page (no API connection yet).

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import Reset from './pages/ResetPassword';
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<Navigate to="/login" />} />
+      <Route path="/reset" element={<Reset />} />
     </Routes>
   );
 }
