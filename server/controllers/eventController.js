5944e5f34528cae4cd83894f70987c83b42c1932
emifirmat <emifirmat_cp@hotmail.com>
2025-05-27 16:09:35 +0100

    Added concurrently configuration to run both server in one CLI.

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -29,7 +29,7 @@ const getEvents = (req, res) => {
 };
 
 const createEvent = (req, res) => {
-	const { title, body, image_url, event_time, location, user_id } = req.body;
+	const { title, description, image_url, event_time, location, user_id } = req.body;
 	const created_at = new Date().toISOString();
 	db.run(`INSERT INTO events (title, description, image_url, event_time, location, user_id, created_at)
 	 	VALUES (?, ?, ?, ?, ?, ?, ?)`,
