3088d036b8e750c4c8b225fb6e64396a73a4c1d3
Dariia <50766469+daria861@users.noreply.github.com>
2025-05-28 14:08:25 +0100

    Update Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import ExternalEventList from '../../components/ExternalEventList/ExternalEventList';
 import UserEventsList from '../../components/userEventsList/userEventsList.jsx';
 import './Home.css';
-import './Home.css';
 
 export default function Home() {
     return (
