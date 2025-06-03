27436410488b873d25d88f91b598cc9ac6e4c8a7
Odhran Gavin <odhrangavin@gmail.com>
2025-06-03 12:53:56 +0100

    fixing more fave bugs

--- server/controllers/favesController.js
+++ server/controllers/favesController.js
@@ -1,9 +1,10 @@
 const db = require('../db');
 
 const getFave = (req, res) => {
-	let id = req.params.id;
-	db.get(`SELECT * FROM faves WHERE id = ?`,
-		[id],
+	let event_id = req.params.id;
+	let user_id = req.user.id
+	db.get(`SELECT * FROM faves WHERE event_id = ? AND user_id = ?`,
+		[event_id, user_id],
 		function (err, row) {
 			res.json({user: row});
 		});
@@ -11,7 +12,7 @@ const getFave = (req, res) => {
 
 const getFaves = (req, res) => {
 	let id = req.user.id;
-	db.get(`SELECT * FROM faves WHERE user_id = ?`,
+	db.all(`SELECT * FROM faves WHERE user_id = ?`,
 		[id],
 		function (err, row) {
 			res.json({user: row});
@@ -22,7 +23,7 @@ const addFave = (req, res) => {
 	const { event, user_id } = req.body;
 	const created_at = new Date().toISOString();
 	db.run(`INSERT INTO faves (event, user_id, created_at)
-			VALUES (?, ?, ?)`),
+			VALUES (?, ?, ?)`,
 		[event, user_id, created_at],
 		function (err) {
 			if (err) {
@@ -30,11 +31,11 @@ const addFave = (req, res) => {
 				return res.status(500).json({ error: 'Database error' });
 			}
 			res.json(this);
-		}
+		});
 }
 
 const deleteFave = (req, res) => {
-	const event_id = req.params.id;;
+	const event_id = req.params.id;
 	db.run(`DELETE FROM faves WHERE id = ?
 			VALUES (?`),
 		[event_id],
