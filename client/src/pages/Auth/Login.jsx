ff6b2802344d8d989fa54e5787d303bff994ee90
emifirmat <emifirmat_cp@hotmail.com>
2025-06-10 14:38:07 +0100

    Styled auth section

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,16 +1,18 @@
 // login page
 import { Link } from 'react-router-dom';
 import LoginForm from '../../components/Auth/LoginForm';
+import './Auth.scss';
 
 export default function Login() {
     return (
-        <div>
+        <>
             <h2>Welcome to ArtistsApp</h2>
-			<p>Show the world what you can do!</p>
-			<br />
-            <h2>Log In</h2>
-            { <LoginForm />}
-            <p>Forgotten your password? <Link to="/forgot-password">Click here</Link></p>
-        </div>
+            <p>Show the world what you can do!</p>
+            <div className='auth-section'>
+                <h2>Log In</h2>
+                { <LoginForm />}
+                <p>Forgotten your password? <Link to="/forgot-password">Click here</Link></p>
+            </div>
+        </>
     );
 }
--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,13 +1,16 @@
 // register page
 import RegisterForm from '../../components/Auth/RegisterForm';
+import './Auth.scss';
 
 export default function Register() {
 	return (
-		<div>
+		<>
 			<h2>Welcome to ArtistsApp</h2>
 			<p>Show the world what you can do!</p>
-			<br />
-			< RegisterForm />
-		</div>
+			<div className='auth-section'>
+				<h2>Register</h2>
+				< RegisterForm />
+			</div>
+		</>
 	);
 }
